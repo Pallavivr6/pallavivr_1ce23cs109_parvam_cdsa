815ec1853edf684591afc46f15330bc7776a9e9a
Pallavivr6 <pallavivrmurthy@gmail.com>
2024-11-06 16:08:44 +0530

    Add files via upload

---
+++ constant.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+    int n=5;
+    printf("%d",n);
+}//constant time- O(1)s
---
+++ format.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    printf("Octal number : %o",n);
+    printf("\nHexa-decimal : %x",n);
+}
